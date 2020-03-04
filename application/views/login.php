7751b6716d393dd52d540c0d2b159e40bb448b37
Hai Nguyen <hai.nguyen@cofoundervp.com>
2020-03-04 15:15:02 +0700

    Update text

--- application/views/login.php
+++ application/views/login.php
@@ -73,7 +73,7 @@
           </div>
         </form>
 
-        <a href="<?php echo base_url() ?>forgotPassword">Forgot Password?</a><br>
+        <a href="<?php echo base_url() ?>forgotPassword">Forgot Password</a><br>
         
       </div><!-- /.login-box-body -->
     </div><!-- /.login-box -->
